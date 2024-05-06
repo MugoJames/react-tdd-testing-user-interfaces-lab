01ccb8d8f793e2e26b21f6d5e56df0f032418787
James <jamonjoroge816@gmail.com>
2024-05-06 08:00:23 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, I'm James Mugo</h1>
+    <h2>About Me</h2>
+    <p>I am pursuing a career in Software Development. I am very passionate about the work and am eager to learn as much as I can</p>
+
+   <div> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1eX5qktXzct1UFD9u1tKHj3ygoPdu9Ywp0w&usqp=CAU" alt="Picture" />
+    </div>
+    <a name="linkedIn" href="https://www.linkedin.com/in/mugoJames">LinkedIn</a>
+    <div></div>
+    <a name="github" href="https://github.com/MugoJames">GitHub</a>
+  </div>;
 }
 
 export default App;
